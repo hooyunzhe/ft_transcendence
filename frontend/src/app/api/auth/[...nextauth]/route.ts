40a8bc9dd4e6f3c0122cfdd101688ee23789dd67
Hoo Yun Zhe <yunzhescientist@gmail.com>
2023-05-23 20:59:49 +0800

    chore: Testing callbacks

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
-import NextAuth, { NextAuthOptions } from 'next-auth';
+import NextAuth, { Account, NextAuthOptions, Profile } from 'next-auth';
+import { JWT } from 'next-auth/jwt';
 import FortyTwoProvider from 'next-auth/providers/42-school';
 
 export const authOptions: NextAuthOptions = {
@@ -8,6 +9,25 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.FT_OAUTH_SECRET ?? '',
     }),
   ],
+  callbacks: {
+    async jwt({ token, account, profile }) {
+      // console.log(token);
+      // console.log('----------------');
+      // console.log(account);
+      if (token && account && profile) {
+        token.refresh_token = account.refresh_token;
+        token.image = profile.image;
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      // if (session?) {
+      //   session.refresh_token = token.refresh_token;
+      //   session.image = token.image;
+      // }
+      return session;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
