568dccd30d34938db256b8dca0fee1fd867364e8
Hoo Yun Zhe <yunzhescientist@gmail.com>
2023-05-27 20:02:45 +0800

    feat: Implemented online status logic in gateway

--- backend/src/friends/friends.gateway.ts
+++ backend/src/friends/friends.gateway.ts
@@ -1,14 +1,42 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 
-@WebSocketGateway()
-export class FriendsGateway {
-  @SubscribeMessage('connection')
-  handleConnection(): string {
-    return 'client connected!';
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+  namespace: 'gateway/friends',
+})
+export class FriendsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
+  connectedUsers = new Set();
+
+  handleConnection(client: Socket) {
+    const user_id = Number(client.handshake.query['id']);
+    this.connectedUsers.add(user_id);
+    client.broadcast.emit('newConnection', user_id);
   }
 
-  @SubscribeMessage('test')
-  test(): string {
-    return 'works!';
+  handleDisconnect(client: Socket) {
+    const user_id = Number(client.handshake.query['id']);
+    this.connectedUsers.delete(user_id);
+    client.broadcast.emit('newDisconnect', user_id);
+  }
+
+  @SubscribeMessage('checkStatus')
+  checkStatus(@MessageBody('users') users: number[]) {
+    const statuses = users.map((user) => ({
+      user_id: user,
+      connected: this.connectedUsers.has(user),
+    }));
+
+    return statuses;
   }
 }
--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,11 +1,9 @@
-import { Achievement } from 'src/achievements/entities/achievement.entity';
 import { UserAchievement } from 'src/user_achievements/entities/user_achievement.entity';
 import {
   Entity,
   Column,
   PrimaryGeneratedColumn,
   CreateDateColumn,
-  ManyToMany,
   OneToMany,
 } from 'typeorm';
 import { ChannelMember } from 'src/channel_members/entities/channel_member.entity';
@@ -24,9 +22,6 @@ export class User {
 
   @CreateDateColumn()
   date_of_creation: Date;
-
-  @Column({ default: 'offline' })
-  status: string;
 
   @OneToMany(() => ChannelMember, (channelMember) => channelMember.user)
   channelMembers: ChannelMember[];
