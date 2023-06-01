181f2ae1f94c284f649c22845dd40fb97a4f39a9
Hoo Yun Zhe <yunzhescientist@gmail.com>
2023-06-01 20:12:33 +0800

    chore: Testing new request in frontend

--- frontend/src/components/FriendList.tsx
+++ frontend/src/components/FriendList.tsx
@@ -11,6 +11,13 @@ export function FriendList() {
   const [friends, setFriends] = useState<Friend[]>([]);
   const [selectedFriend, setSelectedFriend] = useState(0);
 
+  const current_user: User = {
+    id: 4,
+    username: 'Test',
+    refresh_token: 'test_token',
+    date_of_creation: new Date(),
+  };
+
   useEffect(() => {
     async function getFriend() {
       const data = JSON.parse(
@@ -21,13 +28,6 @@ export function FriendList() {
       setFriends(data);
     }
     getFriend();
-
-    let current_user: User = {
-      id: 4,
-      username: 'Test',
-      refresh_token: 'test_token',
-      date_of_creation: new Date(),
-    };
 
     function handleNewRequest(sender: User) {
       setFriends((friends) => {
@@ -58,7 +58,24 @@ export function FriendList() {
       justifyContent='center'
       spacing={1}
     >
-      <Button variant='contained'>Add Friend</Button>
+      <Button
+        variant='contained'
+        onClick={() => {
+          callAPI('POST', 'friends', {
+            outgoing_id: 4,
+            incoming_id: 1,
+            status: 'invited',
+          });
+          friends_socket.emit('newRequest', {
+            sender: current_user,
+            receiver: {
+              id: 1,
+            },
+          });
+        }}
+      >
+        Add Friend
+      </Button>
       {friends.map((obj: Friend, index: number) => (
         <FriendDisplay
           key={index}
