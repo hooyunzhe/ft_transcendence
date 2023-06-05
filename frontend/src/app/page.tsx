1bddfcf0bf9dc3b8f6349d899f98b7c910f0adee
42EuLee <lokeulee@gmail.com>
2023-06-05 16:35:41 +0800

    chore: Halfway on implementing adding channel function

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,12 @@
+import ChannelHeader from '@/components/ChannelHeader';
 import { ChannelList } from '@/components/ChannelList';
 import React, { useState } from 'react';
 export default function Home() {
-  //@ts-expect-error Server Component
-  return <ChannelList API={'channels'}></ChannelList>;
+  return (
+    <>
+    <ChannelHeader></ChannelHeader>
+    {/* @ts-expect-error Server Component  */}
+    <ChannelList API={'channels'}></ChannelList>
+  </>
+  );
 }
--- frontend/src/components/ChannelList.tsx
+++ frontend/src/components/ChannelList.tsx
@@ -2,15 +2,40 @@
 import List from '@mui/material/List';
 import { Channel, ChannelVar } from './Channel';
 import call_API from '@/lib/call_API';
+import { Button, FormControl, Grid, IconButton, TextField } from '@mui/material';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import { useEffect, useState } from 'react';
+
 
 export async function ChannelList({ API }: { API: string }) {
+
+  const [message, setMessage] = useState('');
+
   const data = await call_API(API);
   console.log('channel');
   return (
-    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+    <>
+      <Grid sx={{ width: '100%', maxWidth: 360}} xs={8} item>
+        <FormControl>
+          <TextField
+            onChange={(event: any) => {
+              setMessage(event.target.value);
+              console.log('changed');
+            }}
+            value={message}
+            label='Add Channel Here...'
+            variant='outlined'
+          />
+        </FormControl>
+        <IconButton>
+          <AddCircleOutlineIcon />
+        </IconButton>
+      </Grid>
+      <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
       {data.map((obj: ChannelVar, index: number) => (
         <Channel {...obj}></Channel>
-      ))}
-    </List>
+        ))}
+      </List>
+    </>
   );
 }
