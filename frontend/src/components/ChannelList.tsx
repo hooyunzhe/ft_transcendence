15730fc52801a82df54820d7ce04298c511712dd
42EuLee <lokeulee@gmail.com>
2023-06-05 16:47:38 +0800

    chore: Updating callAPI to latest version/ Added UseEffect for getting channels

--- frontend/src/components/ChannelList.tsx
+++ frontend/src/components/ChannelList.tsx
@@ -1,18 +1,27 @@
 'use client';
 import List from '@mui/material/List';
 import { Channel, ChannelVar } from './Channel';
-import call_API from '@/lib/call_API';
 import { Button, FormControl, Grid, IconButton, TextField } from '@mui/material';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import { useEffect, useState } from 'react';
+import callAPI from '@/lib/callAPI';
 
 
-export async function ChannelList({ API }: { API: string }) {
+export async function ChannelList({ channelAPI }: { channelAPI: string }) {
 
   const [message, setMessage] = useState('');
+  const [channels, setChannels] = useState([]);
 
-  const data = await call_API(API);
-  console.log('channel');
+  useEffect(() => {
+    async function getChannels() {
+      const messageData = JSON.parse(await callAPI('GET', channelAPI));
+      setChannels(messageData);
+      console.log(messageData);
+    }
+    getChannels();
+  }, []);
+  
+
   return (
     <>
       <Grid sx={{ width: '100%', maxWidth: 360}} xs={8} item>
@@ -32,7 +41,7 @@ export async function ChannelList({ API }: { API: string }) {
         </IconButton>
       </Grid>
       <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-      {data.map((obj: ChannelVar, index: number) => (
+      {channels.map((obj: ChannelVar, index: number) => (
         <Channel {...obj}></Channel>
         ))}
       </List>
