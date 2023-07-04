5a9ff63aa376865ca01c6c6ce5b301a11b69e023
hanswong42 <hansdhwong@gmail.com>
2023-07-04 14:12:07 +0800

    feat: added matchmaking service template

--- backend/src/matchmaking/matchmaking.module.ts
+++ backend/src/matchmaking/matchmaking.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MatchmakingGateway } from './matchmaking.gateway';
+import { MatchmakingService } from './matchmaking.service';
 @Module({
-  providers: [MatchmakingGateway],
+  providers: [MatchmakingGateway, MatchmakingService],
 })
 export class MatchmakingModule {}
