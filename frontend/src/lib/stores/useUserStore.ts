1415758453d375f578d548ac03c8f5160ee8f37b
Hoo Yun Zhe <yunzhescientist@gmail.com>
2023-07-07 19:22:54 +0800

    chore: Setup zustand and useUserStore (WIP)

---
+++ frontend/src/lib/stores/useUserStore.ts
@@ -0,0 +1,18 @@
+import { create } from 'zustand';
+import { User } from '@/types/UserTypes';
+
+interface UserStore {
+  user: User;
+}
+
+const useUserStore = create<UserStore>()((set) => ({
+  user: {
+    id: 0,
+    username: '',
+    refresh_token: '',
+    date_of_creation: new Date(),
+  },
+  setUser: (user: User) => set({ user }),
+}));
+
+export default useUserStore;
