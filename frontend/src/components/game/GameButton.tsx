7ff00fd6cf4b66c1ba157607958430728227f4c8
Hans Wong <hansdhwong@gmail.com>
2023-07-11 17:18:01 +0800

    feat: added interactable ready button

---
+++ frontend/src/components/game/GameButton.tsx
@@ -0,0 +1,45 @@
+export class Button {
+  private button: Phaser.GameObjects.Image;
+  private text: Phaser.GameObjects.Text;
+  private buttonClickable: Boolean;
+  constructor(
+    x: number,
+    y: number,
+    label: string,
+    scene: Phaser.Scene,
+    ready: string,
+    unready: string,
+    timeout: number,
+  ) {
+    this.buttonClickable = true;
+    this.button = scene.add
+      .image(x, y, ready)
+      .setOrigin(0.5)
+      .setScale(0.1)
+      .setInteractive({ useHandCursor: true })
+      .on('pointerover', () => this.text.setStyle({ fill: '#f39c12' }))
+      .on('pointerout', () => this.text.setStyle({ fill: '#FFF' }))
+      .on('pointerdown', () => {
+        if (this.buttonClickable) {
+          this.buttonClickable = false;
+          if (this.button.texture.key === ready) {
+            this.button.setTexture(unready);
+            this.text.setText('Ready');
+          } else {
+            this.button.setTexture(ready);
+            this.text.setText('Unready');
+          }
+        }
+        setTimeout(() => {
+          this.buttonClickable = true;
+        }, timeout);
+      });
+    this.text = scene.add.text(x, y, label).setOrigin(0.5).setPadding(10);
+    // .setInteractive({ useHandCursor: true })
+    // .on('pointerover', () => this.text.setStyle({ fill: '#f39c12' }))
+    // .on('pointerout', () => this.text.setStyle({ fill: '#FFF' }));
+    // // .on('pointerdown', () => callback())
+    // .on('pointerover', () => button.setStyle({ fill: '#f39c12' }))
+    // .on('pointerout', () => button.setStyle({ fill: '#FFF' }));
+  }
+}
