3d7bbfd8e6ba122e206fc9932501498a9f924d80
Hoo Yun Zhe <yunzhescientist@gmail.com>
2023-07-13 14:49:45 +0800

    feat: Added action to setup friend socket in FriendStore

--- frontend/src/lib/stores/useFriendStore.ts
+++ frontend/src/lib/stores/useFriendStore.ts
@@ -1,5 +1,7 @@
 import { create } from 'zustand';
 import { Friend, FriendStatus } from '@/types/FriendTypes';
+import { Socket } from 'socket.io-client';
+import { User } from '@/types/UserTypes';
 
 interface FriendStore {
   data: {
@@ -14,41 +16,71 @@ interface FriendStore {
       new_status: FriendStatus,
     ) => void;
     deleteFriend: (incoming_id: number) => void;
+    setupFriendSocket: (friendSocket: Socket) => void;
   };
+}
+
+type StoreSetter = (
+  helper: (state: FriendStore) => Partial<FriendStore>,
+) => void;
+
+function addFriend(set: StoreSetter, friend: Friend) {
+  set(({ data }) => ({ data: { friends: [...data.friends, friend] } }));
+}
+
+function changeFriend(
+  set: StoreSetter,
+  incoming_id: number,
+  current_status: FriendStatus,
+  new_status: FriendStatus,
+) {
+  set(({ data }) => ({
+    data: {
+      friends: data.friends.map((friend) => {
+        if (
+          friend.incoming_friend.id === incoming_id &&
+          friend.status === current_status
+        ) {
+          friend.status = new_status;
+        }
+        return friend;
+      }),
+    },
+  }));
+}
+
+function deleteFriend(set: StoreSetter, incoming_id: number) {
+  set(({ data }) => ({
+    data: {
+      friends: data.friends.filter(
+        (friend) => friend.incoming_friend.id !== incoming_id,
+      ),
+    },
+  }));
+}
+
+function setupFriendSocket(set: StoreSetter, friendSocket: Socket) {
+  friendSocket.on('newRequest', (request: Friend) => addFriend(set, request));
+  friendSocket.on('deleteRequest', (sender: User) =>
+    deleteFriend(set, sender.id),
+  );
+  friendSocket.on('acceptRequest', (sender: User) =>
+    changeFriend(set, sender.id, FriendStatus.INVITED, FriendStatus.FRIENDS),
+  );
+  friendSocket.on('rejectRequest', (sender: User) =>
+    deleteFriend(set, sender.id),
+  );
 }
 
 const useFriendStore = create<FriendStore>()((set) => ({
   data: { friends: [] },
   actions: {
     setFriends: (friends) => set({ data: { friends } }),
-    addFriend: (friend) =>
-      set(({ data }) => ({
-        data: {
-          friends: [...data.friends, friend],
-        },
-      })),
+    addFriend: (friend) => addFriend(set, friend),
     changeFriend: (incoming_id, current_status, new_status) =>
-      set(({ data }) => ({
-        data: {
-          friends: data.friends.map((friend) => {
-            if (
-              friend.incoming_friend.id === incoming_id &&
-              friend.status === current_status
-            ) {
-              friend.status = new_status;
-            }
-            return friend;
-          }),
-        },
-      })),
-    deleteFriend: (incoming_id) =>
-      set(({ data }) => ({
-        data: {
-          friends: data.friends.filter(
-            (friend) => friend.incoming_friend.id !== incoming_id,
-          ),
-        },
-      })),
+      changeFriend(set, incoming_id, current_status, new_status),
+    deleteFriend: (incoming_id) => deleteFriend(set, incoming_id),
+    setupFriendSocket: (friendSocket) => setupFriendSocket(set, friendSocket),
   },
 }));
 
